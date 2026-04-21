7fb1758fc516712aa622b991744d4582c518f81d
Angel Lacret <alacret@gmail.com>
2026-04-21 18:17:12 -0400

    export headers keys

--- http_py/request_logger/__init__.py
+++ http_py/request_logger/__init__.py
@@ -2,9 +2,15 @@ from http_py.request_logger.services import (
     ConsoleRequestLoggerMiddleware,
     DatabaseRequestLoggerMiddleware,
 )
+from http_py.request_logger.constants import (
+    REQUEST_LOGGER_HEADER,
+    REQUEST_LOGGER_CACHE_HEADER,
+)
 
 
 __all__ = [
     "ConsoleRequestLoggerMiddleware",
     "DatabaseRequestLoggerMiddleware",
+    "REQUEST_LOGGER_HEADER",
+    "REQUEST_LOGGER_CACHE_HEADER",
 ]
